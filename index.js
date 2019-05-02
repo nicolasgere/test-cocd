532548bb898334c6cde48445db19a9e07a404c5c
nicolasgere <nicolas.gerelm@gmail.com>
2019-05-02 14:34:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello Wo'))
+app.get('/', (req, res) => res.send('Hello Wor'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
